5decac57ff9f513da9841992d232c461e878a946
Drashti Acharya <drashtiacharya1206>
2026-08-16 18:25:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function App(){
+  return(
+    <div>
+      <h1>Welcome to React</h1>
+      <h2>My first react application</h2>
+      <p>
+        this is a simple react application created using JSX and functional components.
+      </p>
+      <p>react makes it easy to build user interfaces.</p>
+    </div>
+  );
+};
+export default App;
